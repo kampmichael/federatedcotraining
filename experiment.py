5d29c61c438c9992e8c8faed1c294618f1052df9
Amr Abourayya <61755893+Amrusama@users.noreply.github.com>
2024-02-29 17:27:00 +0100

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -27,13 +27,8 @@ from datasets.Heart_Disease_Statlog import *
 from datasets.adultincome import *
 from datasets.mushrooms import *
 from datasets.covtype import *
-
 from clients.rulefitClients import *
 from clients.xgboostClients import *
-
-available_clients = getAvailableClasses(Client)
-available_datasets = getAvailableClasses(Dataset)
-
 from clients import *
 from datasets import *
 import aggregators
@@ -43,38 +38,30 @@ from xor import *
 import copy
 import random
 import tensorflow as tf
+import wandb
+from wandb.keras import WandbMetricsLogger, WandbModelCheckpoint
 
+available_clients = getAvailableClasses(Client)
+available_datasets = getAvailableClasses(Dataset)
 torch.cuda.empty_cache()
+
 #set the random seeds
 random.seed(hash("setting random seeds") % 2**32 - 1)
 np.random.seed(hash("improves reproducibility") % 2**32 - 1)
 
 #Weight and Bises login
-import wandb
-from wandb.keras import WandbMetricsLogger, WandbModelCheckpoint
 wandb.login(key='999ae259390532c6c283ea338b365a2e9b489d78')
 
-
-
-
-#available_clients = ['PaperKerasCIFARNet']
-#for name, obj in inspect.getmembers(clients):
-#    if inspect.isclass(obj):
-#        available_clients.append(name)
-#available_datasets = ['CIFAR10Keras']
-#for name, obj in inspect.getmembers(datasets):
-#    if inspect.isclass(obj):
-#        available_datasets.append(name)
 available_aggregators = []
 for name, obj in inspect.getmembers(aggregators):
     if inspect.isclass(obj):
         available_aggregators.append(name)
         
 #set the parameters
-parser = argparse.ArgumentParser(description='FedCT Experiment Suite')
+parser = argparse.ArgumentParser(description='AIM-HI Experiment Suite')
 parser.add_argument('--method', type=str, default='centralized',
-                    choices=['centralized', 'FedCT', 'FL', 'DP-FL','FL-DP','FedCT-DP','DD','Per-FedCT'],
-                    help='Federated learing method to be used (default: FedCT)')
+                    choices=['centralized', 'FedCT', 'FL','FL-DP','FedCT-DP','DD','FedCT-diffclass','PATE','DP-PATE'],
+                    help='Federated learing method to be used (default: AIMHI)')
 parser.add_argument('--num-clients', type=int, default=1,
                     help='Number of clients in federated network (default: 1)')
 parser.add_argument('--num-rounds', type=int, default=10,
@@ -89,8 +76,12 @@ parser.add_argument('--num-samples-per-client', type=int, default=-1,
                     help='Number of samples each client has available, -1 means the training set is equally divided (default: -1)')
 parser.add_argument('--num-unlabeled', type=int, default=0,
                     help='Number of unlabeled samples, has to be smaller than dataset size (default: 0)')
+
 parser.add_argument('--client', type=str, default=list(available_clients.keys())[0], choices = available_clients,
                     help='Client type used for training (default: '+list(available_clients.keys())[0]+')')
+#parser.add_argument('--client', type=str, default=list(available_clients.keys())[0],
+ #                   help='Client type used for training (default: '+list(available_clients.keys())[0]+')')
+
 parser.add_argument('--dataset', type=str,  default=list(available_datasets.keys())[0], choices = available_datasets,
                     help='dataset type used for training (default: '+list(available_datasets.keys())[0]+')')
 parser.add_argument('--aggregator', type=str, default=available_aggregators[1], choices = available_aggregators,
@@ -109,7 +100,7 @@ parser.add_argument('--lmbda', type=float, default=1,
                     help='lmbda is a regularization parameter for personalized distributed co-training')
 parser.add_argument('--distill-period', type=int, default=1,
                     help='distill period of (default: 2)')
-args = parser.parse_args()    
+args = parser.parse_args()     
 
 cuda = torch.device('cuda:2')
 ### Initialize ###
@@ -152,8 +143,7 @@ if args.method=="DD":
     name="DD"
 if args.method=="Per-FedCT":
     name="Personalized FedCT"
-#args.consensus='average'
-# Start a run, tracking hyperparameters
+    
 wandb.init(
 
     project=name,
@@ -224,12 +214,18 @@ elif args.method == "FedCT": #label sharing
             Xtrain, ytrain = dataset.getNextLocalBatch(i, args.train_batch_size)
             if (t+1) % args.comm_period == 0 and yconsensus is not None:
                 Xtrain, ytrain = np.concatenate((Xtrain, dataset.Xunlabeled)), np.concatenate((ytrain, yconsensus))
-            client.train(Xtrain, ytrain)
+            if args.dataset=="IMDB":
+                client.train_llm(Xtrain, ytrain)
+            else:
+                client.train(Xtrain, ytrain)
         if (t+1) % args.comm_period == 0:
             print("Comm-round ",(t+1))
             #params = []
             for client in clients:
-                predictions.append(client.predict(dataset.Xunlabeled))                
+                if args.dataset=="IMDB":
+                    predictions.append(client.predict_llm(dataset.Xunlabeled))
+                else:
+                    predictions.append(client.predict(dataset.Xunlabeled))              
                 #params.append(client.getParameters())
             #agg_params = aggregate(params)
             predictions = np.array(predictions)
@@ -281,6 +277,41 @@ elif args.method == "FedCT-DP": #label sharing
         if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
             log_errors(clients, dataset, error_measure, error_log, t)
             log_models(clients, t, exp_path)
+
+elif args.method == "FedCT-diffclass": #label sharing
+    #clients = []
+    #print(available_clients.keys())
+    client_names = ['XGBoostClassifier', 'sklearnNeuralNetwork', 'RandomforestClassifier', 'RuleFitClassifier']
+    clients = [available_clients[client_names[i % len(client_names)]]() for i in range(args.num_clients)]
+    print("Clients are", clients)
+    #print('num of clinet is ',args.num_clients)
+    #breakpoint()
+    predictions = None
+    for t in range(args.num_rounds):
+        if isinstance(predictions, list):
+            predictions = np.array(predictions)
+        yconsensus = getConsensusLabels(predictions, args.consensus) #we could avoid that with the same if statement as in the concatenation below, but I choose less code over efficiency ATM
+        predictions = []
+        for i,client in enumerate(clients):
+            Xtrain, ytrain = dataset.getNextLocalBatch(i, args.train_batch_size)
+            if (t+1) % args.comm_period == 0 and yconsensus is not None:
+                Xtrain, ytrain = np.concatenate((Xtrain, dataset.Xunlabeled)), np.concatenate((ytrain, yconsensus))
+            client.train(Xtrain, ytrain)
+        if (t+1) % args.comm_period == 0:
+            print("Comm-round ",(t+1))
+            #params = []
+            for client in clients:
+                predictions.append(client.predict(dataset.Xunlabeled))                
+                #params.append(client.getParameters())
+            #agg_params = aggregate(params)
+            predictions = np.array(predictions)
+            #comm_log[t] = {"params":params.copy(),"agg":agg_params,"clients":params}
+            comm_log[t] = {"predictions" : predictions.copy(), "consensus_labels" : getConsensusLabels(predictions, args.consensus)}
+        if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
+            log_errors(clients, dataset, error_measure, error_log, t)
+            log_models(clients, t, exp_path)
+
+
 elif args.method == "FL": #model aggregation (e.g., averaging)
     clients = []
     for _ in range(args.num_clients):
@@ -289,7 +320,10 @@ elif args.method == "FL": #model aggregation (e.g., averaging)
     for t in range(args.num_rounds):
         for i,client in enumerate(clients):
             Xtrain, ytrain = dataset.getNextLocalBatch(i, args.train_batch_size)
-            client.train(Xtrain, ytrain)
+            if args.dataset=="IMDB":
+                client.train_llm(Xtrain, ytrain)
+            else:
+                client.train(Xtrain, ytrain)
         if (t+1) % args.comm_period == 0:
             print("Comm-round ",(t+1))
             params = []
@@ -360,7 +394,100 @@ elif args.method == "DD":
         if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
             log_errors(clients, dataset, error_measure, error_log, t)
             log_models(clients, t, exp_path)
+            
+elif args.method == "PATE":
+    clients = []
+    for _ in range(args.num_clients):
+        client = client_class()
+        clients.append(client)
 
+    # Training local models on local private data
+    for t in range(args.num_rounds):
+        for i, client in enumerate(clients):
+            Xtrain, ytrain = dataset.getNextLocalBatch(i, args.train_batch_size)
+            client.train(Xtrain, ytrain)
+        print(" Teachers learning process ....")
+        if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
+            log_errors(clients, dataset, error_measure, error_log, t)
+            log_models(clients, t, exp_path)
+
+    # Communication for generating pseudo labels
+    predictions = np.array([client.predict(dataset.Xunlabeled) for client in clients])
+    yconsensus = getConsensusLabels(predictions, args.consensus)
+    
+    print("Student learning process started")
+    student_model = client_class()  
+    student_train_errors, student_test_errors  = [], []
+    # Train student model for 1000 epochs. This could be a hyperparameter as well but I just set it to 1000 for now
+    for t in range(1000):
+        student_model.train(dataset.Xunlabeled, yconsensus)
+
+        # Log student errors (you might want to adjust the frequency)
+        if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
+            ytrainpred = student_model.predict(dataset.Xunlabeled)
+            student_train_errors.append(error_measure(yconsensus, ytrainpred))
+            ypred = student_model.predict(dataset.Xtest)
+            student_test_errors.append(error_measure(dataset.ytest, ypred))
+            print("Student round", t + 1, ": student_train error", student_train_errors[-1], ", student_test_error", student_test_errors[-1])
+            # Log errors at each round
+            wandb.log({"student_test_acc": student_test_errors[-1], "student_train_acc": student_train_errors[-1], "student_round": t + 1})
+
+
+elif args.method == "DP-PATE":
+    clients = []
+    for _ in range(args.num_clients):
+        client = client_class()
+        clients.append(client)
+
+    epsilon = 2  # Set your desired privacy parameter
+    b_teachers = 1.0 / epsilon  # Laplace noise scale for teachers
+
+    # Training local models on local private data
+    for t in range(args.num_rounds):
+        for i, client in enumerate(clients):
+            Xtrain, ytrain = dataset.getNextLocalBatch(i, args.train_batch_size)
+            client.train(Xtrain, ytrain)
+        print(" Teachers learning process ....")
+        if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
+            log_errors(clients, dataset, error_measure, error_log, t)
+            log_models(clients, t, exp_path)
+
+    # Communication for generating pseudo labels
+    # Label aggregation with privacy
+    predictions = np.array([client.predict(dataset.Xunlabeled) for client in clients])
+    print(predictions)
+
+    # Add Laplace noise to each class count to introduce ambiguity
+    num_classes=2
+    noisy_counts_per_class = []
+    for class_label in range(num_classes):  # assuming num_classes is 10 for Fashion-MNIST
+        noisy_counts = add_laplace_noise(np.sum(predictions == class_label, axis=0), b_teachers)
+        noisy_counts_per_class.append(noisy_counts)
+    # Add Laplace noise to vote counts to introduce ambiguity
+    # Stack the noisy counts for all classes
+    noisy_counts = np.vstack(noisy_counts_per_class)
+    # Choose the label with the highest noisy count as the consensus label
+    yconsensus = np.argmax(noisy_counts, axis=0)
+    print("yconsensus len ", yconsensus.shape)
+    print("prediction",predictions.shape)
+    print("noisy_counts", noisy_counts.shape)
+
+    print("Student learning process started")
+    student_model = client_class()  
+    student_train_errors, student_test_errors  = [], []
+    # Train student model for 500 epochs. This could be a hyperparameter as well but I just set it to 1000 for now
+    for t in range(1000):
+        student_model.train(dataset.Xunlabeled, yconsensus)
+
+        # Log student errors (you might want to adjust the frequency)
+        if args.evaluation_rounds == 1 or (t + 1) % args.evaluation_rounds == 0:
+            ytrainpred = student_model.predict(dataset.Xunlabeled)
+            student_train_errors.append(error_measure(yconsensus, ytrainpred))
+            ypred = student_model.predict(dataset.Xtest)
+            student_test_errors.append(error_measure(dataset.ytest, ypred))
+            print("Student round", t + 1, ": student_train error", student_train_errors[-1], ", student_test_error", student_test_errors[-1])
+            # Log errors at each round
+            wandb.log({"student_test_acc": student_test_errors[-1], "student_train_acc": student_train_errors[-1], "student_round": t + 1})
 
 else:
     print("ERROR: method not recognized: ",args.method)
